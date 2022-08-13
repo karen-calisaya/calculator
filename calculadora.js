d9acae8356c445b68c1320b28f326e09dce74f86
Karen Calisaya <karenmcalisaya@gmail.com>
2022-08-13 16:18:55 -0300

    change folder

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,14 @@
-let sumar = require("./sumar");   //lo guardo en variable para poder utilizarlo.
+let sumar = require("./operaciones/sumar");   //lo guardo en variable para poder utilizarlo.
 console.log(sumar(20, 3));       
 
 
-let restar = require("./restar");
+let restar = require("./operaciones/restar");
 console.log(restar(40, 20));
 
-let multiplicar = require("./multiplicar");
+let multiplicar = require("./operaciones/multiplicar");
 console.log(multiplicar(10, 1));
 
-let dividir = require("./dividir");
+let dividir = require("./operaciones/dividir");
 console.log(dividir(0, 20));
 
 //hay diferentes formas de hacerlo:
